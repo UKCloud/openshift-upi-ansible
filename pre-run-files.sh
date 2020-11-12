f5c4d944473fd5bbf68017a76a4d51d95cefc263
Kieran O'Neill <koneill@ukcloud.com>
2020-11-12 14:09:36 +0000

    clouds.yaml template complete

--- pre-run-files.sh
+++ pre-run-files.sh
@@ -7,3 +7,7 @@ tar -xzf openshift-install-mac-4.6.4.tar.gz
 curl https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/4.6/4.6.1/rhcos-4.6.1-x86_64-openstack.x86_64.qcow2.gz -o rhcos-4.6.1-x86_64-openstack.x86_64.qcow2.gz
 
 gunzip rhcos-4.6.1-x86_64-openstack.x86_64.qcow2.gz
+
+ssh-keygen -t rsa -b 4096 -N '' -f openshift_key
+
+cp openshift_key.pub ../files/pubkey.pem

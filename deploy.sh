7fdf405412e7c98fa90fc7efc6f5ab29c43eafbd
Kieran O'Neill <koneill@ukcloud.com>
2020-11-17 10:36:01 +0000

    added bastion stage

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 ansible-playbook -e @vars.yml generate-inventory.yaml
 
 ansible-playbook -i inventory.yaml -e @vars.yml deploy.yaml
+
+openshift-install wait-for bootstrap-complete
+
+ansible-playbook -i inventory.yaml down-bootstrap.yaml
+
+ansible-playbook -i inventory.yaml bastion.yaml

d72553e2ba7eacaaa16d384b25bfe8779df6e838
Kieran O'Neill <koneill@ukcloud.com>
2020-12-10 16:22:32 +0000

    added ssh key to deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 set -xe
 
+ssh-keygen -t rsa -b 4096 -N '' -f ssh-key/openshift_key
+
+cp ssh-key/openshift_key.pub files/pubkey.pem
+
 ansible-galaxy collection install -r requirements.yml
 
 ansible-playbook -e @vars.yml generate-clouds.yaml

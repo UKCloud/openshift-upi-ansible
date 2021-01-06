550caa7a71aa3fd487cc24df1cc2269365e0fbd1
Kieran O'Neill <koneill@ukcloud.com>
2021-01-06 13:21:43 +0000

    updated code references to master

--- controlplane.sh
+++ controlplane.sh
@@ -1,10 +1,10 @@
 for index in $(seq 0 2); do
-    MASTER_HOSTNAME="$INFRA_ID-controlplane-$index\n"
+    CONTROLPLANE_HOSTNAME="$INFRA_ID-controlplane-$index\n"
     python -c "import base64, json, sys;
 ignition = json.load(sys.stdin);
 storage = ignition.get('storage', {});
 files = storage.get('files', []);
-files.append({'path': '/etc/hostname', 'mode': 420, 'contents': {'source': 'data:text/plain;charset=utf-8;base64,' + base64.standard_b64encode(b'$MASTER_HOSTNAME').decode().strip(), 'verification': {}}, 'filesystem': 'root'});
+files.append({'path': '/etc/hostname', 'mode': 420, 'contents': {'source': 'data:text/plain;charset=utf-8;base64,' + base64.standard_b64encode(b'$CONTROLPLANE_HOSTNAME').decode().strip(), 'verification': {}}, 'filesystem': 'root'});
 storage['files'] = files;
 ignition['storage'] = storage
 json.dump(ignition, sys.stdout)" <master.ign >"$INFRA_ID-controlplane-$index-ignition.json"
